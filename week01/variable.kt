3dc48ea52e6d0d1f259f5f3b66c49a8f881fde2b
Francisco I. Rodriguez <firodriguez@dcccd.edu>
2023-08-03 10:54:06 -0500

    Create variable.kt

---
+++ week01/variable.kt
@@ -0,0 +1,17 @@
+fun main() {
+
+    val myAge: Int = 25
+    var averageAge: Double = 30.0
+    val testNumber: Int = 1347
+    val evenOdd = (testNumber % 2)
+    averageAge = ((averageAge + myAge) / 2)
+    println("Average age = $averageAge")
+    println(evenOdd)
+
+    var answer = 0
+    answer = answer + 1
+    // answer = answer + 10
+    answer+=10
+    answer = answer * 10
+    print(answer)
+}
